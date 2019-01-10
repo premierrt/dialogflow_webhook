a3c8121e1a6c47350606795b5f6e789ccccd0dc4
dom_linux <dom_linux@ddddd.xy>
2019-01-11 00:10:40 +0100

    procesor intent - ok

---
+++ src/main/java/dialogflow/simple_post/IntentServiceProcessor.java
@@ -0,0 +1,9 @@
+package dialogflow.simple_post;
+
+import org.json.JSONObject;
+
+public interface IntentServiceProcessor {
+
+	public FullfillmentResponseDTO processIntent(JSONObject jsonObject);
+	
+}

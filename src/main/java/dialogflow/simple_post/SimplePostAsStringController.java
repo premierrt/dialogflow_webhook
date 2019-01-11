f6d5b310310e9e4072e7d1bc4bfb7c77c132814b
dom_linux <dom_linux@ddddd.xy>
2019-01-11 22:45:45 +0100

    dodanie wywolania service do controller

--- src/main/java/dialogflow/simple_post/SimplePostAsStringController.java
+++ src/main/java/dialogflow/simple_post/SimplePostAsStringController.java
@@ -2,6 +2,7 @@ package dialogflow.simple_post;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,9 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 public class SimplePostAsStringController {
 
+	@Autowired
+	IntentServiceProcessor intentServiceProcessor;
+	
 	@PostMapping("/webhook")
 	/**
 	 * https://dialogflow.com/docs/reference/api-v2/rest/Shared.Types/WebhookRequest
@@ -24,16 +28,17 @@ public class SimplePostAsStringController {
 	 * @return
 	 */
 	public ResponseEntity<?> processDialogFlowPost(@RequestBody String json){
-	
+	//!!!poprawic obsluge bledow
+		FullfillmentResponseDTO fullfillmentResponseDTO= new FullfillmentResponseDTO();
 		log.info("--------- "+json );
 		try {
 			JSONObject jsonObject = new JSONObject(json.toString());
-			
+			fullfillmentResponseDTO =intentServiceProcessor.processIntent(jsonObject);
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return ResponseEntity.status(HttpStatus.OK).build();
+		return ResponseEntity.status(HttpStatus.OK).body(fullfillmentResponseDTO);
 		
 	}
 }

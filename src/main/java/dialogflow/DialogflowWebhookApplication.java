881f60a9f6b57fdc239351dc566db9e3d4899d98
dom_linux <dom_linux@ddddd.xy>
2019-01-16 00:01:37 +0100

    nots

--- src/main/java/dialogflow/DialogflowWebhookApplication.java
+++ src/main/java/dialogflow/DialogflowWebhookApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DialogflowWebhookApplication {
 
+	//https://stackoverflow.com/questions/34172888/difference-between-bean-and-autowired
+		
 	public static void main(String[] args) {
 		SpringApplication.run(DialogflowWebhookApplication.class, args);
 	}

c670056a9b2cf6850b1f1dcf3a6e56aee9845365
rafal_ubuntu_praca <rafal@gmail>
2019-01-16 13:53:32 +0100

    zmiana interface

--- src/main/java/dialogflow/intentprocessing/ToiletIntentProcessor.java
+++ src/main/java/dialogflow/intentprocessing/ToiletIntentProcessor.java
@@ -1,11 +1,13 @@
 package dialogflow.intentprocessing;
 
+import org.json.JSONObject;
+
 import dialogflow.simple_post.FullfillmentResponseDTO;
 
 public class ToiletIntentProcessor implements IntenetProcessor {
 
 	@Override
-	public FullfillmentResponseDTO processIntent() {
+	public FullfillmentResponseDTO processIntent(JSONObject jsonObject) {
 		// TODO Auto-generated method stub
 		return null;
 	}

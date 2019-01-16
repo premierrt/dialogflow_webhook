26478427c8daec20e33a25b83c115d80547abc26
rafal_ubuntu_praca <rafal@gmail>
2019-01-16 13:51:11 +0100

    interface

---
+++ src/main/java/dialogflow/intentprocessing/IntenetProcessor.java
@@ -0,0 +1,8 @@
+package dialogflow.intentprocessing;
+
+import dialogflow.simple_post.FullfillmentResponseDTO;
+
+public interface IntenetProcessor {
+
+	public FullfillmentResponseDTO processIntent();
+}
---
+++ src/main/java/dialogflow/intentprocessing/ToiletIntentProcessor.java
@@ -0,0 +1,13 @@
+package dialogflow.intentprocessing;
+
+import dialogflow.simple_post.FullfillmentResponseDTO;
+
+public class ToiletIntentProcessor implements IntenetProcessor {
+
+	@Override
+	public FullfillmentResponseDTO processIntent() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
